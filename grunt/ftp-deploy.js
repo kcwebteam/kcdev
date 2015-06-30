8886a8885e11d31f4825d3175bf951522506e695
Alex Aragon <Alex.Aragon@kingcounty.gov>
2015-06-30 13:56:51 -0700

    updating ftp session details

--- grunt/ftp-deploy.js
+++ grunt/ftp-deploy.js
@@ -6,7 +6,7 @@ module.exports = {
       authKey: 'key1'
     },
     src: ['<%= public %>/' ],
-    dest: 'kcproto.kingcounty.gov/bs3/',
+    dest: 'kcproto',
     exclusions: ['<%= public %>/html','']
   }
 };
