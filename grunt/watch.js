dbc2123fb3ded0f769957c08eef465a6f3505732
Alex Aragon <Alex.Aragon@kingcounty.gov>
2016-06-06 13:47:47 -0700

    Removing connect from Watch task

--- grunt/watch.js
+++ grunt/watch.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   spec: {
     files: ['<%= src %>/jade/<%= targetFile %>.jade'],
-    tasks: ['jade:spec', 'connect'],
+    tasks: ['jade:spec'],
     options: {
       atBegin: true
     }
