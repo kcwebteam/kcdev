38ab097c76cdb8870d3177ef00cb003a542bab3a
Aragon <aragonwa@gmail.com>
2018-05-11 08:59:23 -0700

    Fix footer not collapsing

--- src/js/lib/footer-collapse-resize.js
+++ src/js/lib/footer-collapse-resize.js
@@ -1,8 +1,8 @@
 $(window).resize(function () {
   // Collapse window on resize
   if ($('#nav-xs').css('display') === 'none') {
-    $('#footer-nav .col-sm-4 ul').collapse('show');
+    $('#footer-nav').find('[class^="col-sm-"]').find('ul').collapse('show');
   } else {
-    $('#footer-nav .col-sm-4 ul').collapse('hide');
+    $('#footer-nav').find('[class^="col-sm-"]').find('ul').collapse('hide');
   }
 }).resize(); // trigger resize handlers
